450b0ab761452b488dfa23374dbbed003262e041
fuc-Git <3022303284@qq.com>
2025-05-13 10:39:46 +0800

    Update onehub.js

--- onehub.js
+++ onehub.js
@@ -1,1 +1,14 @@
-const express=require('express');
+const express =require('express');
+
+const app=express();
+
+app.get('/user',(req,res)=>{
+    const params=req.query;
+    for(let i in params){
+        console.log(`参数:${i} 参数值:${params[i]}`);
+    }
+    res.send(`<h1>已成功解析GET请求参数</h1>`)
+});
+app.listen(3000,()=>{
+    console.log('启动！');
+})
